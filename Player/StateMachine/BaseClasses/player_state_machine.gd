d7055916b57a1790929828f47901038238f77811
Aria Williams <awill9579@gmail.com>
2026-07-26 00:18:34 -0500

    Added in movement/look disabling

--- Player/StateMachine/BaseClasses/player_state_machine.gd
+++ Player/StateMachine/BaseClasses/player_state_machine.gd
@@ -2,7 +2,6 @@ class_name PlayerStateMachine extends StateMachine
 
 @export var player : Player
 
-@export var look_enabled : bool = true
 
 func _ready() -> void:
 	if player == null:
@@ -22,16 +21,17 @@ func setup_states() -> void:
 
 ## Head rotation for first-person controls
 func _unhandled_input(event: InputEvent) -> void:
-	if !look_enabled:
+	if event.is_action_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	
+	if event is InputEventMouseButton and event.pressed:
+		if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+	if !player.look_enabled:
 		return
 	
 	if event is InputEventMouseMotion:
 		player.rotate_y(-event.relative.x * player.mouse_sensitivity)
 		player.head.rotate_x(-event.relative.y * player.mouse_sensitivity)
 		player.head.rotation.x = clamp(player.head.rotation.x, deg_to_rad(-89), deg_to_rad(89))
-
-	if event.is_action_pressed("ui_cancel"):
-		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	if event is InputEventMouseButton and event.pressed:
-		if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
-			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
--- Player/player.gd
+++ Player/player.gd
@@ -4,14 +4,34 @@ class_name Player extends CharacterBody3D
 @export var mouse_sensitivity : float = 0.003
 ## Value used in delta-adjusted calculations
 @export var acceleration: float = 10.0
+@export var look_enabled : bool = true
+@export var movement_enabled : bool = true
 
+## Rotation helper to make head rotation not break :)
 @onready var head: Node3D = %Head
-
 ## The player state machine contains all logic for player movement & look logic
 @onready var player_state_machine: PlayerStateMachine = %PlayerStateMachine
 
-const SPEED = 5.0
-const MOUSE_SENSITIVITY : float = 0.003
-
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
+## Head rotation for first-person controls
+func _unhandled_input(event: InputEvent) -> void:
+	# show cursor in first person mode
+	if event.is_action_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	
+	# capture cursor on mouse click
+	if event is InputEventMouseButton and event.pressed:
+		if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+	if !look_enabled:
+		return
+	
+	# rotate head
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		head.rotate_x(-event.relative.y * mouse_sensitivity)
+		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-89), deg_to_rad(89))
