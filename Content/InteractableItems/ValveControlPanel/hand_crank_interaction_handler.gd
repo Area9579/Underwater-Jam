ef71bec4a48ee819739502a6f0200aa60b280804
Aria Williams <awill9579@gmail.com>
2026-07-26 22:20:34 -0500

    Basic Hand Crank

--- Content/InteractableItems/ValveControlPanel/hand_crank_interaction_handler.gd
+++ Content/InteractableItems/ValveControlPanel/hand_crank_interaction_handler.gd
@@ -5,16 +5,41 @@ var hand_crank : HandCrank
 
 func _init(interactable_node : HandCrank) -> void:
 	super(interactable_node)
+	
 	if interactable_node is not HandCrank:
 		printerr("%s: Interactable node: %s is not a HandCrank" % [self, interactable_node])
 		return
+	
 	hand_crank = interactable_node
 
 
+
 func is_correct_input_given() -> bool:
-	if Input.is_action_just_pressed("interact"):
+	if Input.is_action_pressed("interact") || Input.is_action_pressed("alt_interact"):
 		return true
 	return false
+
+
+
+func do_interaction(_entity : FirstPersonEntityController) -> void:
+	is_interacting = true
+	var tween = create_tween()
+	
+	if Input.is_action_pressed("interact"):
+		tween.tween_method(add_to_rotation, 0, 1, 0.01)
+	elif Input.is_action_pressed("alt_interact"):
+		tween.tween_method(subtract_from_rotation, 0, 1, 0.01)
+	
+	await tween.finished
+	is_interacting = false
+
+
+func add_to_rotation(value : float) -> void:
+	hand_crank.rotation.y += deg_to_rad(value)
+
+
+func subtract_from_rotation(value : float) -> void:
+	hand_crank.rotation.y -= deg_to_rad(value)
 
 
 func fade_in_hover_effect() -> void:
@@ -23,12 +48,3 @@ func fade_in_hover_effect() -> void:
 
 func fade_out_hover_effect() -> void:
 	pass
-
-
-func do_interaction(_entity : FirstPersonEntityController) -> void:
-	is_interacting = true
-	var curr_rotation = hand_crank.global_rotation
-	var tween = create_tween()
-	tween.tween_property(hand_crank, "global_rotation", Vector3(curr_rotation.x, curr_rotation.y + deg_to_rad(90), curr_rotation.z), 0.5)
-	await tween.finished
-	is_interacting = false
