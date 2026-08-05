0e46a4b5ffa83cd7b62298cf5ecd020938e7b067
Aria Williams <awill9579@gmail.com>
2026-08-04 22:12:44 -0500

    fix camera jitter

--- Gym/gym.gd
+++ Gym/gym.gd
@@ -12,7 +12,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	# 'E' key
 	if Input.is_action_just_pressed("debug_toggle"):
 		player.look_enabled = !player.look_enabled
